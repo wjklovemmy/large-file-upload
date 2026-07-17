addd40811aa5e36a2f6ee570e9b0bd36d4b5813d
王建坤 <18910846521@163.com>
2026-07-17 17:09:58 +0800

    init

---
+++ src/main/java/com/example/largefileupload/LargeFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.example.largefileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeFileUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LargeFileUploadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/largefileupload/LargeFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.largefileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeFileUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
